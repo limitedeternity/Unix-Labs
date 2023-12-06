1268d806bc1585ecdde7700e5b9edb39213d9c97
Vyacheslav Bespalov <vbespalov@ptsecurity.com>
2023-12-06 23:01:34 +0300

    assist.sh

---
+++ src/assist.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+if [[ $# -eq 0 ]]; then
+    echo "Usage: $0 cmd [args ...]"
+    exit 1
+fi
+
+command=$1
+shift
+args=$@
+
+temp_file=$(mktemp)
+function clean_exit { rm -f $temp_file; exit 0; }
+
+trap clean_exit INT
+$command $args > $temp_file < /dev/null
+
+return_code=$?
+printf "Command finished with code: %d\n" "$return_code"
+
+if [[ -s $temp_file ]]; then
+    while true; do
+        echo -n "Display output in: (e/p/o/n/?) "
+        read option
+
+        case $option in
+            e)
+                ${EDITOR:-nano} $temp_file
+                break ;;
+
+            p)
+                ${PAGER:-less} $temp_file
+                break ;;
+
+            o)
+                cat $temp_file
+                break ;;
+
+            n)
+                break ;;
+
+            *)
+                echo "e - open in editor"
+                echo "p - open in pager"
+                echo "o - print to stdout"
+                echo "n - do nothing"
+                ;;
+        esac
+    done
+fi
+
+clean_exit
