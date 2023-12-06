40fdd4f1d859ba84d249e1c08e4793f213ca05b2
Vyacheslav Bespalov <vbespalov@ptsecurity.com>
2023-12-07 00:34:26 +0300

    Fixes

--- src/homeless.sh
+++ src/homeless.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 awk -F: '$6 == "/dev/null" {print $1 "\t" $3}' /etc/passwd | sort -k2n
-
-echo -n "Total: "
-awk -F: '$6 == "/dev/null" {count++} END {print count}' /etc/passwd
+printf "Total:\t"
+awk -F: '$6 == "/dev/null" {count++} END {if (count == 0) print 0; else print count}' /etc/passwd
