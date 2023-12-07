5446736c40491ec6469cb7c8868e577d1ae38a9b
Vyacheslav Bespalov <vbespalov@ptsecurity.com>
2023-12-07 11:39:06 +0300

    Fixes

--- src/lines.sh
+++ src/lines.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-find . -type f -exec wc -l {} + | awk '{ sum += $1 } END { print sum }'
+find . -type f -exec wc -l {} + | awk '{sum += $1} END {if (sum == 0) print 0; else print sum}'
